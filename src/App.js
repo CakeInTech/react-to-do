18f438582c9d3eb9a74ab9336979c5d8b2d5b685
MohamedCK <abdulhakimm280@gmail.com>
2022-10-31 19:56:18 +0200

    Create a todo app using class combonents

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Component file
+import Orji from "./components/TodoContainer";
+//stylesheet
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Orji />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const headerStyle = {
+    fontSize: "6rem",
+    fontWeight: "600",
+    marginBottom: "2rem",
+    color: "#ececec",
+    textTransform: "lowercase",
+    textAlign: "center",
+    padding: "20px 0",
+    lineHeight: "1.5rem"
+}
+const Header = () => {
+    return (
+        <header>
+            <h1
+                style={headerStyle}
+            >
+                todos</h1>
+        </header>
+    )
+}
+
+export default Header;
